958fcc20228d40d2aac690afbcc7ae9da396a3b2
Ahmed Elsawaf <aelsawaf50@gmail.com>
2024-12-15 19:19:20 +0200

    chekOut path edit

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import "flowbite/dist/flowbite.js";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
